ecacd47ff653c032a7c3c2fc1caae92878517b96
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2022-12-17 13:08:33 +0700

    Seminar 7

---
+++ Seminar_7/Task_49/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса 
+// нечётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+       {
+        for (int j=0;j<array.GetLength(1);j++) 
+            Console.Write($"{array[i,j],3}\t");
+        Console.WriteLine();
+       }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+       for (int j=0;j<array.GetLength(1);j++) 
+            array[i,j] = new Random().Next(1,11);
+}
+
+void SquareArray(int[,] array)
+{
+    for (int i=1;i<array.GetLength(0);i+=2) // нечетные позиции индексов (i+2 при i=1)
+       {
+        for (int j=1;j<array.GetLength(1);j+=2) 
+             array[i,j] = array[i,j]*array[i,j]; 
+       }
+}
+
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows,cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SquareArray(array);
+PrintArray(array);
---
+++ Seminar_7/Task_51/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на 
+// главной диагонали с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void Fill2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+void PrintTable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j],3}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Summ(int[,] table)
+{
+    int sum = 0;
+    int sum2 = 0;
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+
+            if (i == j) sum = table[i, j] + sum;
+            if (table.GetLength(0) - i - 1 == j) sum2 = table[i, j] + sum2;
+
+        }
+    }
+    Console.WriteLine($"Сумма значений диагонали слева на право: {sum}");
+    Console.WriteLine($"Сумма значений диагонали справа на лево: {sum2}");
+}
+
+Console.WriteLine("Колличество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Колличество столбцов");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] table = new int[rows, colums];
+Fill2DArray(table);
+PrintTable(table);
+Summ(table);
